e06f36b7d1f88aa2a0ccf029676955d3da979a3a
Tachera Sasi <tacherasasi@gmail.com>
2025-11-12 15:06:18 +0300

    Update configuration.ts

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -2,6 +2,6 @@ export default () => ({
   port: parseInt(String(process.env.PORT), 10) || 3000,
   database: {
     host: process.env.DATABASE_HOST,
-    port: parseInt(String(process.env.DATABASE_PORT), 10) || 5432
+    port: parseInt(String(process.env.DATABASE_URL), 10) || 5432
   },
 });
